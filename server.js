8005e9984d6ebe17089b5cedaa0f66cea18331bc
watchara.p <watchara.pongsri@thomsonreuters.com>
2018-07-02 18:38:27 +0700

    add project

---
+++ server.js
@@ -0,0 +1,102 @@
+const hapi = require('hapi');
+const mongoose = require('mongoose');
+const Painting = require('./models/Painting');
+
+const { graphqlHapi, graphiqlHapi } = require('apollo-server-hapi');
+const schema = require('./graphql/schema');
+
+const Inert = require('inert');
+const Vision = require('vision');
+const HapiSwagger = require('hapi-swagger');
+const Pack = require('./package');
+
+const server = hapi.server({
+    host: 'localhost',
+    port: 4444
+});
+
+mongoose.connect('mongodb://jib:1234@localhost:27017/graphql_dev');
+// or
+//mongoose.connect('mongodb://localhost:27017/graphql_dev');
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+});
+
+const init = async () => {
+    await server.register([
+		Inert,
+		Vision,
+		{
+			plugin: HapiSwagger,
+			options: {
+				info: {
+					title: 'Paintings API Documentation',
+					version: Pack.version
+				}
+			}
+		}
+	]);
+
+    await server.register({
+		plugin: graphiqlHapi,
+		options: {
+			path: '/graphiql',
+			graphiqlOptions: {
+				endpointURL: '/graphql'
+			},
+			route: {
+				cors: true
+			}
+		}
+	});
+
+	await server.register({
+		plugin: graphqlHapi,
+		options: {
+			path: '/graphql',
+			graphqlOptions: {
+				schema
+			},
+			route: {
+				cors: true
+			}
+		}
+    });
+    server.route([
+		{
+			method: 'GET',
+			path: '/api/v1/paintings',
+			config: {
+				description: 'Get all the paintings',
+				tags: ['api', 'v1', 'painting']
+			},
+			handler: (req, reply) => {
+				return Painting.find();
+			}
+		},
+		{
+			method: 'POST',
+			path: '/api/v1/paintings',
+			config: {
+				description: 'Get a specific painting by ID.',
+				tags: ['api', 'v1', 'painting']
+			},
+			handler: (req, reply) => {
+				const { name, url, technique } = req.payload;
+				const painting = new Painting({
+					name,
+					url,
+					technique
+				});
+
+				return painting.save();
+			}
+		}
+	]); 
+
+	await server.start();
+	console.log(`Server running at: ${server.info.uri}`);
+};
+
+
+init();
